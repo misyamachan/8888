e97d27966fe3de6f5d2b364a3c8262a1683eaa11
misyamachan <120997609+misyamachan@users.noreply.github.com>
2022-12-31 21:27:15 +0900

    load

--- main.js
+++ main.js
@@ -87,3 +87,34 @@ window.addEventListener("load", function () {
         }
     });
 });
+
+$(function () {
+    // ローダー終了
+    function end_loader() {
+        $('.Load').fadeOut(800);
+    }
+    // テキスト表示
+    function show_txt() {
+        $('.Load img').fadeIn(400);
+    }
+    // テキスト非表示
+    function hide_txt() {
+        $('.Load img').fadeOut(400);
+    }
+
+    // タイマー処理
+    $(window).on('load', function () {
+        // 処理①
+        setTimeout(function () {
+            show_txt();
+        }, 1000)
+        // 処理②
+        setTimeout(function () {
+            hide_txt();
+        }, 3500)
+        // 処理③
+        setTimeout(function () {
+            end_loader();
+        }, 4500)
+    })
+})
